d77852e3dba44808e74f91a30651f680eaffc630
Esme Parkinson <parkinson.esme@gmail.com>
2015-08-26 18:20:47 +0100

    added word separation

---
+++ separating-words/wordSeparation.py
@@ -0,0 +1,36 @@
+'''
+Input: user enters string consisting of words without spaces
+Output: each word is printed on a different line.
+
+Favours longer words (first word where there is overlap between two valid words)
+'''
+
+def checkIfReal(word):
+    real = False
+    file = open('words.txt', 'r')
+    word = word.lower()
+    for line in file:
+        if word == line.strip('\n'):
+            real = True
+            break
+    return real
+def keepTestingString(userString, n):
+    testString = userString[:n]
+    #checkIfReal(testString)
+    if checkIfReal(testString):
+        print(testString)
+        userString = userString[n:]
+        iterateOverString(userString)
+    if not checkIfReal(testString):
+        n = n-1
+        keepTestingString(userString, n)
+        
+def iterateOverString(userString):
+    n = len(userString)
+    keepTestingString(userString, n)
+    
+
+#main method
+print('Enter string: ')
+userString=str(input())
+iterateOverString(userString)
