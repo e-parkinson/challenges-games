355479c7cd8c088aef2b61e0c9c65edf15894b64
Esme Parkinson <parkinson.esme@gmail.com>
2015-09-18 07:55:26 +0100

    Make and print a board

---
+++ bots-on-board/makeBoard.py
@@ -0,0 +1,32 @@
+def changeCharAtPos(a, b, char, lsBoard):
+    workingList = lsBoard[a]
+    workingList.insert(b, char)
+    popAt = b + 1
+    workingList.pop(popAt)
+    return lsBoard
+
+print('Game board size n x n.\nValue of n?')
+n = int(input())
+
+lsBoard = [] #prepare rows
+while len(lsBoard) < n:
+    lsBoard.append([])
+
+for i in range(0,len(lsBoard)): #prepare columns
+    workingList = lsBoard[i]
+    while len(workingList) < n:
+        workingList.append('. ')
+
+print('Starting position (2 integers \'row column\' space separated)?')
+a, b = input().split()
+a,b = int(a),int(b)
+lsBoard = changeCharAtPos(a, b, '@ ', lsBoard)
+
+print('Goal position (2 integers \'row column\' space separated)?')
+x, y = input().split()
+x,y = int(x),int(y)
+lsBoard = changeCharAtPos(x, y, '0 ', lsBoard)
+
+print('\nStarting state: ')
+for i in range(0, len(lsBoard)):
+    print(*lsBoard[i], sep='')
